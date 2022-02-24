91e8f5ed093c63b363184a54c04c5eff2a6e7bb8
arielchauca <69734387+arielchauca@users.noreply.github.com>
2022-02-24 17:19:52 -0300

    Models Orientation Complete

--- api/src/db.js
+++ api/src/db.js
@@ -6,7 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/iunit`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
@@ -36,6 +36,7 @@ const { Candidate, Recruiter } = sequelize.models;
 // Product.hasMany(Reviews);
 Candidate.belongsToMany(Recruiter, { through: 'candidate_recruiter' });
 Recruiter.belongsToMany(Candidate, { through: 'candidate_recruiter' });
+
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
---
+++ api/src/models/Orientation.js
@@ -0,0 +1,10 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  return sequelize.define('Orientation', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};

3fb58c5eedc5f7091e80430f35f861f1b5c9ee3d
Veronica Cesari Carson <verocesari@gmail.com>
2022-03-02 14:19:43 -0300

    Ruta Formation completa

---
+++ api/src/routes/formation.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const router = express.Router();
+
+
+const {Candidate, Formation} = require('../db.js');
+
+
+router.get('/', async (req, res) => {
+    try {
+        const formations = await Formation.findAll({
+            include: [{
+                model: Candidate
+                
+            }]
+        });
+        res.json(formations);
+    } catch (error) {
+        res.status(400).json({
+            message: error.message
+        });
+    }
+});
+
+router.post("/", async (req, res, next) => {
+
+    const {title, institution, ubication, status, starting_date, ending_date, description, idCandidate} = req.body;
+    try {
+        
+        const formation = await Formation.create({
+            title,
+            institution,
+            ubication,
+            status,
+            starting_date,
+            ending_date,
+            description
+
+        })
+        
+        if(formation && idCandidate){
+            const candidate = await Candidate.findByPk(idCandidate);
+            candidate.addFormation(formation);
+        }
+        res.json({msg: "la formacion se guardo correctamente"})    
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
+
+router.delete("/candidate/:id", async (req, res, next) => {
+    const {id} = req.params;
+    try {
+        const formation = await Formation.findByPk(id);
+        if(formation){
+            await formation.destroy();
+
+            res.json({msg: "la formacion se elimino correctamente"})
+        }else{
+            res.json({msg: "la formacion no existe"})
+        }
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+
+module.exports = router;

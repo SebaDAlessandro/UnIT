cfbb2afd750766ce2e94af5da55c444ed747f970
gisecin <gisecin@gmail.com>
2022-03-02 13:39:54 -0300

    project

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -10,6 +10,7 @@ const candidatesBulkcreate = require('./candidatesBulkcreate')
 const technicalskill = require('./technicalSkill')
 const softskill = require('./softSkill')
 const orientation = require('./orientation')
+const projectexperience = require('./projectexperience')
 
 
 const router = Router();
@@ -25,6 +26,7 @@ router.use('/candidatesBulk', candidatesBulkcreate);
 router.use('/technicalskill', technicalskill);
 router.use('/softskill', softskill);
 router.use('/orientation', orientation);
+router.use('/projectexperience', projectexperience)
 
 
 

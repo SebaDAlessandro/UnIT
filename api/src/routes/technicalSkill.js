63c9bd68d033f128565ad69f2faef6ed0af8b224
asdelaf <asanchezdelaf@hotmail.com>
2022-03-02 11:22:28 -0300

    rutas de orientation y skills

---
+++ api/src/routes/technicalSkill.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+
+
+const {Candidate, Technicalskills} = require('../db.js');
+
+router.post("/", async (req, res, next) => {
+
+    const {technicalskills, idCandidate} = req.body;
+    try {
+        
+        const ts = await Technicalskills.create({
+            technicalskills
+        })
+        
+        if(ts && idCandidate){
+            const candidate = await Candidate.findByPk(idCandidate);
+            candidate.addTechnicalskills(ts);
+        }
+        res.json({msg: "la technicalskill se guardo correctamente"})    
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+module.exports = router;

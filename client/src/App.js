fcb25cae9849dbbd8d73d29799c107d6f3af2e4f
Javier Córdoba <80362946+Javier-Cordobaq@users.noreply.github.com>
2022-03-03 20:26:18 -0500

    Publicando front

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-import Cards from './components/Cards/Cards'
+import Home from './components/Home/Home'
 import FormHome from './components/HomeForm/HomeForm'
 import LogIn from './components/LogIn/LogIn'
 import FormCandidate from './components/FormCandidate/FormCandidate';
 import FormRecluiter from './components/FormRecluiter/FormRecluiter';
+import About from './components/About/About'
 import { Route, Routes, Link  } from 'react-router-dom'
 
 function App() {
@@ -20,7 +21,9 @@ function App() {
 
       <Route exact path='/formc' element={<FormCandidate/>}/>
   
-      <Route exact path='/home' element={<Cards/>}/>
+      <Route exact path='/home' element={<Home/>}/>
+      
+      <Route exact path='/about' element={<About/>}/>
 
     </Routes>
 

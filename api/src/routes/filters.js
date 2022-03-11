1528d593eebf756663ad56a006b69ea2990525e6
arielchauca <69734387+arielchauca@users.noreply.github.com>
2022-03-11 12:26:42 -0300

    filtrado todos sin name

--- api/src/routes/filters.js
+++ api/src/routes/filters.js
@@ -1,206 +1,150 @@
 const express = require('express');
 const router = express.Router();
-const {Op} = require('sequelize');
-
-const {Candidate, Language, Contacted, Technicalskills, Project_experience, Softskill, Orientation} = require('../db.js');
 
 
-router.get('/location/:location', async(req, res, next) => {
-    const {location} = req.params;
-    try {
-        const candidates = await Candidate.findAll({
-            include: [
-                {model: Language}, {model: Contacted}, {model: Technicalskills}, {model: Softskill}, {model: Project_experience}, {model: Orientation}
-            ],
-            where: {
-                location: location
-            }
-        })
-        res.send(candidates);
-    } catch (error) {
-        next(error)
-    }
-})
-
-
-router.get('/language/:language', async(req, res, next) => {
-    const {language} = req.params;
-    try {
-        const candidates = await Candidate.findAll({
-            include: [
-                {model: Language, where: {language: language}}, {model: Contacted}, {model: Technicalskills}, {model: Softskill}, {model: Project_experience}, {model: Orientation}
-            ]
-        })
-        res.send(candidates);
-    } catch (error) {
-        next(error)
-    }
-})
-
-
-// router.get('/Tskill/:tskill', async(req, res, next) => {
-//     const {tskill} = req.params;
-//     try {
-//         const candidates = await Candidate.findAll({
-//             include: [
-//                 {model: Language}, {model: Contacted}, {model: Technicalskills, where: {technicalskills: tskill}}, {model: Softskill}, {model: Project_experience}, {model: Orientation}
-//             ]
-//         })
-//         res.send(candidates);
-//     } catch (error) {
-//         next(error)
-//     }
-// })
-
-
-// router.get('/Tskill', async (req, res, next) => {
-//     const { id } = req.body;
-//     try{
-//         const candidatos = await Candidate.findAll({
-//              where:{
-//                 include:[
-//                     {model: Technicalskills,
-//                         [Op.overlap]: {
-//                             id
-//                         }
-//                     }]
-//              }   
-//         })
-//         res.json(candidatos)
-//     }catch(error){
-//         next(error)
-//     }
-// })
-
-
-
-
-router.get('/Sskill/:sskill', async(req, res, next) => {
-    const {sskill} = req.params;
-    try {
-        const candidates = await Candidate.findAll({
-            include: [
-                {model: Language}, {model: Contacted}, {model: Technicalskills}, {model: Softskill, where: {soft_skill: sskill}}, {model: Project_experience}, {model: Orientation}
-            ]
-        })
-        res.send(candidates);
-    } catch (error) {
-        next(error)
-    }
-})
-
-
-router.get('/Orientation/:orientation', async(req, res, next) => {
-    const {orientation} = req.params;
-    try {
-        const candidates = await Candidate.findAll({
-            include: [
-                {model: Language}, {model: Contacted}, {model: Technicalskills}, {model: Softskill}, {model: Project_experience}, {model: Orientation, where: {name: orientation}}
-            ]
-        })
-        res.send(candidates);
-    } catch (error) {
-        next(error)
-    }
-})
+const {Candidate, Language, Contacted, Technicalskills, Project_experience, Softskill, Orientation, Op} = require('../db.js');
 
 
 
 router.get("/total", async (req, res, next) => {
-    const {location, language, tskill, sskill, orientation} = req.body;
+
+    // language, tskill, sskill son arrays [ ] <---- 
+
+    const {name, location, language, tskill, sskill, orientation} = req.body;
 
     try {
-        
+
         if(location){
-            var candidates = await Candidate.findAll({ 
+            var candidates = await Candidate.findAll({
                 where: {location: location},
-                include: [
-                    {model: Language}, {model: Contacted}, {model: Technicalskills}, {model: Softskill}, {model: Project_experience}, {model: Orientation}
-                ]
             })
         }else{
             var candidates = await Candidate.findAll();
         }
 
-        
-        if(language && candidates){
-            var aux = [];
-            for(let i=0; i<candidates.length; i++){
+        //filtrador por name en proceso!! <-----
+
+        // if(name && candidates){
+        //     let aux = [];
+        //     console.log("hola")
+        //     for(let i=0; i<candidates.length; i++){
                 
+        //      let flag = candidates[i].name.toUpperCase()
+        //      let expresion = /(`${name}` \d+(\.\d)*)/i;
+        //         if(flag.match(expresion)){
+        //             aux.push(candidates[i])
+        //         }
+                
+        //     }
+        //     candidates = aux;
+
+        // }
+
+
+        if(language && candidates){
+            let aux = [];
+            for(let i=0; i<candidates.length; i++){
+
                 var a = await Candidate.findByPk(candidates[i].id,{
                     include: [
-                        {model: Language, where: {language: language}}, {model: Contacted}, {model: Technicalskills}, {model: Softskill}, {model: Project_experience}, {model: Orientation}
+                        {model: Language, where: {language: language}}
                     ]
                 })
-                
+
                 if(a){
                     aux.push(a)
                 }
-                
+
             }
             
-            candidates = aux;
+            candidates = aux.filter(f => {
+                return f.languages.length === language.length
+             })
+             console.log(candidates)
         }
 
         if(tskill && candidates){
-            var aux = [];
+            let aux = [];
             for(let i=0; i<candidates.length; i++){
-                
+
                 var a = await Candidate.findByPk(candidates[i].id,{
                     include: [
-                        {model: Language}, {model: Contacted}, {model: Technicalskills, where: {[Op.and]: [{technicalskills: tskill}]}}, {model: Softskill}, {model: Project_experience}, {model: Orientation}
+                       {model: Technicalskills, where: {technicalskills: tskill}}
                     ]
                 })
-                
+
                 if(a){
                     aux.push(a)
                 }
-                
+
             }
-            
-            candidates = aux;
+         
+            candidates = aux.filter(f => {
+               return f.technicalskills.length === tskill.length
+            })
         }
 
         if(sskill && candidates){
-            var aux = [];
+            let aux = [];
             console.log(candidates.length)
             for(let i=0; i<candidates.length; i++){
-                
+
                 var a = await Candidate.findByPk(candidates[i].id,{
                     include: [
-                        {model: Language}, {model: Contacted}, {model: Technicalskills}, {model: Softskill, where: {soft_skill: sskill}}, {model: Project_experience}, {model: Orientation}
+                       {model: Softskill, where: {soft_skill: sskill}}
                     ]
                 })
-                
+
                 if(a){
                     aux.push(a)
                 }
-                
+
             }
-            
-            candidates = aux;
+
+            candidates = aux.filter(f => {
+                return f.softskills.length === sskill.length
+             })
         }
 
         if(orientation && candidates){
-            var aux = [];
+            let aux = [];
             console.log(candidates.length)
             for(let i=0; i<candidates.length; i++){
-                
+
                 var a = await Candidate.findByPk(candidates[i].id,{
                     include: [
                         {model: Language}, {model: Contacted}, {model: Technicalskills}, {model: Softskill}, {model: Project_experience}, {model: Orientation, where: {name: orientation}}
                     ]
                 })
-                
+
                 if(a){
                     aux.push(a)
                 }
-                
+
             }
-            
-            candidates = aux; 
+
+            candidates = aux;
+
+        }else{
+             if(candidates){
+                let aux = [];
+                
+                for(let i=0; i<candidates.length; i++){
+    
+                    var a = await Candidate.findByPk(candidates[i].id,{
+                        include: [
+                            {model: Language}, {model: Contacted}, {model: Technicalskills}, {model: Softskill}, {model: Project_experience}, {model: Orientation}
+                        ]
+                    })
+    
+                    if(a){
+                        aux.push(a)
+                    }
+                }
+                candidates = aux;
+             }
         }
-        
+
         const contador = candidates.length;
 
         res.json({contador,candidates})

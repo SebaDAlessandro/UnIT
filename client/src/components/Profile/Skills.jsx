5cf12590b47271e531f77b2346ebf122757547ce
Marcos García <86496161+MarcosDemian@users.noreply.github.com>
2022-03-14 22:00:40 -0300

    Miperfil2.0

--- client/src/components/Profile/Skills.jsx
+++ client/src/components/Profile/Skills.jsx
@@ -1,47 +1,87 @@
-import React from "react";
+import * as React from 'react';
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
 import style from './Skills.module.css'
 
-export default function Skills (){
-    return (
-        <div>
-            <h3>Mis Habilidades</h3>
-                <div className="Tecnicas">
-                    <button>Tecnicas</button>
-                    <span>Javascript</span>
-                    <span>React</span>
-                    <span>Css</span>
-                    <span>Java</span>
-                    <span>PHP</span>
-                    <span>Python</span>
-                    <span>HTML</span>
-                    <span>Redux</span>
-                    <span>Node</span>
-                    
-                    <div>
-                        <button>Editar Habilidades</button>
-                    </div>
-                </div>
-                <div className="Blandas">
-                    <button>Blandas</button>
-                    <span>Paciente</span>
-                    <span>Empatíc@</span>
-                    <span>Puntual</span>
-                    <span>Respetuos@</span>
-                    <span>Compañer@</span>
-                    <span>Analista</span>
-                    <span>Autodidacta</span>
-                    <span>Atent@</span>
-                    <span>Adaptabilidad</span>
-                    
-                    <div>
-                        <button>Editar Habilidades</button>
-                    </div>
-                </div>
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
 
-     
-                <div className="work">
-                    
-                </div>
-        </div>
-    )
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
 }
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+export default function BasicTabs() {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div> 
+      <h1 className={style.habilidades}>Mis Habilidades:</h1>
+    <div className={style.container}>
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" className={style.lenguetas}>
+          <Tab label="Blandas" {...a11yProps(0)} />
+          <Tab label="Tecnicas" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+                <span className={style.btn}>Javascript</span>
+                <span className={style.btn}>React</span>
+                <span className={style.btn}>Css</span>
+                <span className={style.btn}>Java</span>
+                <span className={style.btn}>PHP</span>
+                <span className={style.btn}>Python</span>
+                <span className={style.btn}>HTML</span>
+                <span className={style.btn}>Redux</span>
+                <span className={style.btn}>Node</span>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+                <span>Paciente</span>
+                <span>Empatíc@</span>
+                <span>Puntual</span>
+                <span>Respetuos@</span>
+                <span>Compañer@</span>
+                <span>Analista</span>
+                <span>Autodidacta</span>
+                <span>Atent@</span>
+                <span>Adaptabilidad</span>
+      </TabPanel>
+    </Box>
+    </div>
+    </div>
+  );
+}
+        

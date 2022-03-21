ddd8e93aedf40e39a39ecbc8941dd80e8809336e
asdelaf <asanchezdelaf@hotmail.com>
2022-03-21 20:18:40 -0300

    delete index

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ const { conn } = require('./src/db.js');
 //  });
 
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
-    console.log('listening at: ', process.env.PORT); // eslint-disable-line no-console
+  server.listen(3001, () => {
+    console.log('listening at: ', 3001); // eslint-disable-line no-console
   });
 });

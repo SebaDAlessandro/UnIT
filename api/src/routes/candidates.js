670a2142ed6bf182d6ff394f0b07ecdc879f8700
Ruben Emmanuel Zaragoza <72862574+emmazaragoza@users.noreply.github.com>
2022-03-22 10:42:16 -0300

    Update candidates.js

--- api/src/routes/candidates.js
+++ api/src/routes/candidates.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const bcryptjs = require('bcryptjs');
 
-const {Candidate, Language, Contacted, Technicalskills, Project_experience, Softskill, Orientation, Location} = require('../db.js');
+const {Candidate, Language, Contacted, Technicalskills, Project_experience, Softskill, Orientation, Location, Senorit} = require('../db.js');
 
 
 router.get('/', async(req, res, next) => {
